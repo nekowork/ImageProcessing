02fe34854df70a00e3ef467b59fb1ce48ea0c2cd
Mikazuki <45732838+TsumugiWenders@users.noreply.github.com>
2020-04-12 17:15:31 +0900

    Add opt to save setting
    0vo

--- AvatarCapture.py
+++ AvatarCapture.py
@@ -24,30 +24,36 @@ def detect(filename, cascade_file="lbpcascade_animeface.xml"):
         #此处输出裁剪后大小
         face = cv2.resize(face, (256,256))
         #可视化
-        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-        #cv2.imshow("AnimeFaceDetect", image)
-        #cv2.waitKey(0)
-        #save_filename = '%s-%d.jpg' % (os.path.basename(filename).split('.')[0], i)
-        #cv2.imwrite("out/" + save_filename, image)
-        #plt显示
-        cvimg = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        plt.imshow(cvimg)
-        plt.show()
-        #显示裁剪完成图片
-        #cv2.imshow("img",face)
-        #cv2.waitKey(0)
-        #保存裁剪图片打开此注释
-        #save_filename = '%s-%d.jpg' % (os.path.basename(filename).split('.')[0], i)
-        #cv2.imwrite("Data/" + save_filename, face)
+        cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 10)
+        if opt['opencv-val']==1:
+            cv2.namedWindow('AnimeFaceDetect', cv2.WINDOW_KEEPRATIO)
+            cv2.imshow("AnimeFaceDetect", image)
+            cv2.waitKey(0)
+        if opt["PLT-val"]==1:
+            cvimg = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            #plt.title("")
+            plt.imshow(cvimg)
+            plt.show()
+        if opt["save-val-image"]==1:
+            save_filename = '%s-%d.jpg' % (os.path.basename(filename).split('.')[0], i)
+            cv2.imwrite("out/" + save_filename, image)
+        if opt["show-fin-image"]==1:
+            cv2.imshow("img",face)
+            cv2.waitKey(0)
+        if opt["save-fin-image"]==1:
+            save_filename = '%s-%d.jpg' % (os.path.basename(filename).split('.')[0], i)
+            cv2.imwrite("Data/" + save_filename, face)
 
 if __name__ == '__main__':
     if os.path.exists('Data') is False:
         os.makedirs('Data')
     if os.path.exists('out') is False:
         os.makedirs('out')
-    if os.path.exists('IMG') is False:
-        print('file not exists!')
-        exit()
+    opt = {"opencv-val": 0,
+           "PLT-val": 1,
+           "show-fin-image":0,
+           "save-fin-image":0,
+           "save-val-image":0}
     file_list = glob('IMG/*.jpg')
     for filename in file_list:
         try:
@@ -55,4 +61,4 @@ if __name__ == '__main__':
         except:
             print('A Error happy ...')
         else:
-            print(str(filename)+' done !')
+            print(str(filename) + ' done !')
