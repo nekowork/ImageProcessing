a6763e1bc23a65dd52db0436763515378573beeb
Mikazuki <45732838+TsumugiWenders@users.noreply.github.com>
2020-04-14 18:33:32 +0900

    Add plt val show fin img

--- AvatarCapture.py
+++ AvatarCapture.py
@@ -31,7 +31,9 @@ def detect(filename, cascade_file="lbpcascade_animeface.xml"):
             cv2.waitKey(0)
         if opt["PLT-val"]==1:
             cvimg = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            #plt.title("")
+            plt.title(filename)
+            plt.xlabel('X-Px')
+            plt.ylabel('Y-Px')
             plt.imshow(cvimg)
             plt.show()
         if opt["save-val-image"]==1:
@@ -40,6 +42,12 @@ def detect(filename, cascade_file="lbpcascade_animeface.xml"):
         if opt["show-fin-image"]==1:
             cv2.imshow("img",face)
             cv2.waitKey(0)
+        if opt["show-fin-image-plt"]==1:
+            showimg = cv2.cvtColor(face,cv2.COLOR_BGR2RGB)
+            plt.xlabel('X-Px')
+            plt.ylabel('Y-Px')
+            plt.imshow(showimg)
+            plt.show()
         if opt["save-fin-image"]==1:
             save_filename = '%s-%d.jpg' % (os.path.basename(filename).split('.')[0], i)
             cv2.imwrite("Data/" + save_filename, face)
@@ -55,6 +63,7 @@ if __name__ == '__main__':
     opt = {"opencv-val": 0,
            "PLT-val": 1,
            "show-fin-image":0,
+           "show-fin-image-plt": 0,
            "save-fin-image":0,
            "save-val-image":0}
     file_list = glob('IMG/*.jpg')
